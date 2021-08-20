f96d8211b2f78248817f477ffa7915b3da85fc24
RimshaSaleem <rimshatif8@gmail.com>
2021-08-20 07:02:21 -0700

    done

--- src/services/fetchColorService.js
+++ src/services/fetchColorService.js
@@ -2,9 +2,10 @@ import axiosWithAuth from '../helpers/axiosWithAuth';
 
 const fetchColorService =  ()  =>   {
     return axiosWithAuth().get("/colors")
-      .then(res => res)
-      .catch (err =>console.log(err))
+    .then(res => res)
+    .catch (err =>console.log(err))
    
 };
+
 export default fetchColorService;
 
